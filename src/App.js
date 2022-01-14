0f65343521bfbff822bec3a2854955908ddf898e
Conor Clarke <31772275+conorc470@users.noreply.github.com>
2022-01-14 11:19:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import ContactCards from "./ContactCards";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black">
+			<section>
+				<form>
+					<input 
+						type="text"
+						className="ml-20 mt-6 rounded-md p-2"
+						placeholder="Type here to filter results..." 
+					/>
+				</form>	
+			</section>
+			<section>
+				<ContactCards />
+			</section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
