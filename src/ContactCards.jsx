bc5f1b6da6bb332e5e86212461405345f07176ac
Conor Clarke <31772275+conorc470@users.noreply.github.com>
2022-01-14 11:15:10 +0000

    initial commit

---
+++ src/ContactCards.jsx
@@ -0,0 +1,14 @@
+const ContactCards = () => {
+	return (
+		<>
+			<figure>
+				<img alt="user" src="" />
+				<figcaption>
+
+				</figcaption>
+			</figure>
+		</>
+	)
+}
+
+export default ContactCards
