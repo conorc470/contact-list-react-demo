4a3851c8ae0d2385422d67910f7a07f42c6c7e24
Conor Clarke <31772275+conorc470@users.noreply.github.com>
2022-01-14 14:20:31 +0000

    css changes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ const App = () => {
 				<form>
 					<input 
 						type="text"
-						className="ml-20 mt-6 rounded-md p-2"
+						className="ml-20 mt-6 rounded-md p-2 w-4/12"
 						placeholder="Type here to filter results..." 
 					/>
 				</form>	
 			</section>
-			<section>
+			<section className="p-20 grid md:grid-cols-4 sm:grid-cols-1 gap-4">
 				<ContactCards />
 			</section>
     </div>
--- src/ContactCards.jsx
+++ src/ContactCards.jsx
@@ -1,10 +1,116 @@
 const ContactCards = () => {
 	return (
 		<>
-			<figure>
-				<img alt="user" src="https://randomuser.me/api/portraits/men/81.jpg" />
-				<figcaption>
-
+			<figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+				<img 
+				alt="user" 
+				className="w-32 h-32 rounded-full mx-auto"
+				src="https://randomuser.me/api/portraits/men/81.jpg" 
+				/>
+				<figcaption className="text-center mt-5">
+					<p className="text-xl text-gray-800 font-semibold mb-2">
+						Conor Clarke
+					</p>
+					<p className="text-gray-500">conorclarke@gmail.com</p>
+					<p className="text-gray-500">000 000 0000</p>
+				</figcaption>
+			</figure>
+			<figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+				<img 
+				alt="user" 
+				className="w-32 h-32 rounded-full mx-auto"
+				src="https://randomuser.me/api/portraits/men/81.jpg" 
+				/>
+				<figcaption className="text-center mt-5">
+					<p className="text-xl text-gray-800 font-semibold mb-2">
+						Conor Clarke
+					</p>
+					<p className="text-gray-500">conorclarke@gmail.com</p>
+					<p className="text-gray-500">000 000 0000</p>
+				</figcaption>
+			</figure>
+			<figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+				<img 
+				alt="user" 
+				className="w-32 h-32 rounded-full mx-auto"
+				src="https://randomuser.me/api/portraits/men/81.jpg" 
+				/>
+				<figcaption className="text-center mt-5">
+					<p className="text-xl text-gray-800 font-semibold mb-2">
+						Conor Clarke
+					</p>
+					<p className="text-gray-500">conorclarke@gmail.com</p>
+					<p className="text-gray-500">000 000 0000</p>
+				</figcaption>
+			</figure>
+			<figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+				<img 
+				alt="user" 
+				className="w-32 h-32 rounded-full mx-auto"
+				src="https://randomuser.me/api/portraits/men/81.jpg" 
+				/>
+				<figcaption className="text-center mt-5">
+					<p className="text-xl text-gray-800 font-semibold mb-2">
+						Conor Clarke
+					</p>
+					<p className="text-gray-500">conorclarke@gmail.com</p>
+					<p className="text-gray-500">000 000 0000</p>
+				</figcaption>
+			</figure>
+			<figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+				<img 
+				alt="user" 
+				className="w-32 h-32 rounded-full mx-auto"
+				src="https://randomuser.me/api/portraits/men/81.jpg" 
+				/>
+				<figcaption className="text-center mt-5">
+					<p className="text-xl text-gray-800 font-semibold mb-2">
+						Conor Clarke
+					</p>
+					<p className="text-gray-500">conorclarke@gmail.com</p>
+					<p className="text-gray-500">000 000 0000</p>
+				</figcaption>
+			</figure>
+			<figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+				<img 
+				alt="user" 
+				className="w-32 h-32 rounded-full mx-auto"
+				src="https://randomuser.me/api/portraits/men/81.jpg" 
+				/>
+				<figcaption className="text-center mt-5">
+					<p className="text-xl text-gray-800 font-semibold mb-2">
+						Conor Clarke
+					</p>
+					<p className="text-gray-500">conorclarke@gmail.com</p>
+					<p className="text-gray-500">000 000 0000</p>
+				</figcaption>
+			</figure>
+			<figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+				<img 
+				alt="user" 
+				className="w-32 h-32 rounded-full mx-auto"
+				src="https://randomuser.me/api/portraits/men/81.jpg" 
+				/>
+				<figcaption className="text-center mt-5">
+					<p className="text-xl text-gray-800 font-semibold mb-2">
+						Conor Clarke
+					</p>
+					<p className="text-gray-500">conorclarke@gmail.com</p>
+					<p className="text-gray-500">000 000 0000</p>
+				</figcaption>
+			</figure>
+			<figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+				<img 
+				alt="user" 
+				className="w-32 h-32 rounded-full mx-auto"
+				src="https://randomuser.me/api/portraits/men/81.jpg" 
+				/>
+				<figcaption className="text-center mt-5">
+					<p className="text-xl text-gray-800 font-semibold mb-2">
+						Conor Clarke
+					</p>
+					<p className="text-gray-500">conorclarke@gmail.com</p>
+					<p className="text-gray-500">000 000 0000</p>
 				</figcaption>
 			</figure>
 		</>
